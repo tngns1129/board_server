8189a07d8cad1827fbac481a5818f223173b47fa
tngns <tngns1129@naver.com>
2023-10-14 15:56:01 +0900

    docker-compose setting

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-drz*y*b0-al_di2xzp-jk*&#h8t(aqcyugnewh#7_o7zqq^z@i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
