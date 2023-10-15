a8ea59719489779d3dfbb19997360976d2f341f8
tngns <tngns1129@naver.com>
2023-10-16 00:02:34 +0900

    ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -29,7 +29,7 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+sudo mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
