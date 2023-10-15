1be4d43d9b9d6389d64726034cf817b6b17afc8a
tngns <tngns1129@naver.com>
2023-10-16 00:30:07 +0900

    ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,8 +8,8 @@ fi
 domains=(semo962046.duckdns.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="semo962046@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="semo962046@gmail.com" # 유효한 이메일 주소를 추가하는 것이 좋습니다.
+staging=0 # 테스트 중인 경우 요청 제한을 피하려면 1로 설정합니다.
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -17,7 +17,6 @@ if [ -d "$data_path" ]; then
     exit
   fi
 fi
-
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
@@ -29,11 +28,13 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-sudo mkdir -p "$data_path/conf/live/$domains"
-chown -R "ec2-user" "$data_path"
-chgrp -R "ec2-user" "$data_path"
+mkdir -p "$data_path/conf/live/$domains"
+
+# 추가: 데이터 경로 권한 수정
+chown -R ec2-user:ec2-user "$data_path"
+
 docker-compose -f docker-compose.yml run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
@@ -64,7 +65,7 @@ case "$email" in
 esac
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+if [ $staging -eq 1 ]; then staging_arg="--staging"; fi
 
 docker-compose -f docker-compose.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
