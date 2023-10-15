b63896ad5854ef39abc94d34ddf535548e15601c
tngns <tngns1129@naver.com>
2023-10-16 00:32:45 +0900

    ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
+# Docker Compose가 설치되었는지 확인
 if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+  echo '오류: docker-compose가 설치되지 않았습니다.' >&2
   exit 1
 fi
 
+# 도메인 설정
 domains=(semo962046.duckdns.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="semo962046@gmail.com" # 유효한 이메일 주소를 추가하는 것이 좋습니다.
-staging=0 # 테스트 중인 경우 요청 제한을 피하려면 1로 설정합니다.
+staging=0 # 테스트 중인 경우 1로 설정하여 요청 제한을 피할 수 있습니다.
 
+# 데이터 경로 확인
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "해당 도메인에 대한 기존 데이터가 발견되었습니다. 계속하고 기존 인증서를 대체하시겠습니까? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
 fi
 
+# TLS 매개변수 다운로드
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
+  echo "TLS 매개변수를 다운로드합니다..."
   mkdir -p "$data_path/conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
   curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
+# 더미 인증서 생성
+echo "더미 인증서를 생성합니다..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-
-# 추가: 데이터 경로 권한 수정
-chown -R ec2-user:ec2-user "$data_path"
-
+chown -R ec2-user "$data_path"
 docker-compose -f docker-compose.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 \
     -keyout '$path/privkey.pem' \
@@ -40,31 +42,31 @@ docker-compose -f docker-compose.yml run --rm --entrypoint "\
     -subj '/CN=localhost'" certbot
 echo
 
-echo "### Starting nginx ..."
+# Nginx 시작
+echo "Nginx를 시작합니다..."
 docker-compose -f docker-compose.yml up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
+# 이전 인증서 제거
+echo "이전 인증서를 제거합니다..."
 docker-compose -f docker-compose.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
 echo
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
+# Let's Encrypt 인증서 요청
+echo "Let's Encrypt 인증서를 요청합니다..."
 domain_args=""
 for domain in "${domains[@]}"; do
   domain_args="$domain_args -d $domain"
 done
 
-# Select appropriate email arg
 case "$email" in
   "") email_arg="--register-unsafely-without-email" ;;
   *) email_arg="--email $email" ;;
 esac
 
-# Enable staging mode if needed
 if [ $staging -eq 1 ]; then staging_arg="--staging"; fi
 
 docker-compose -f docker-compose.yml run --rm --entrypoint "\
@@ -77,5 +79,8 @@ docker-compose -f docker-compose.yml run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading nginx ..."
+# Nginx 재시작
+echo "Nginx를 다시 시작합니다..."
 docker-compose -f docker-compose.yml exec nginx nginx -s reload
+
+echo "모든 작업이 완료되었습니다."
